745d910ec1aef8043bf6c4b03db29f5cea980915
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2021-04-02 20:38:44 +0700

    lehuynhgiang update code

--- source_api/WebSocketLib/ConnectionObject.cs
+++ source_api/WebSocketLib/ConnectionObject.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net.WebSockets;
 
 namespace WebSocketLib
 {
-    class ConnectionObject :Dictionary<Guid, WebSocket>
+    delegate void SentEventHandler(object sender, EventArgs eventArgs);
+    delegate void ReceivedEventHandler(object sender, EventArgs eventArgs);
+    class ConnectionObject
     {
         private WebSocket webSocket;
+        private byte[] databuffer;
+
+        #region Properties
         public WebSocket WebSocket { get; set; }
+        public event SentEventHandler OnSent;
+        public event ReceivedEventHandler OnReceived;
+        #endregion Properties
+
+        #region Methods
         bool IsSynchronize()
         {
             return default(bool);
@@ -17,5 +25,16 @@ namespace WebSocketLib
         {
             return default(bool);
         }
+        int Send()
+        {
+            OnSent?.Invoke(this, new EventArgs());
+            return 0;
+        }
+        int Receive()
+        {
+            OnReceived?.Invoke(this, new EventArgs());
+            return 0;
+        }
+        #endregion Methods
     }
 }
--- source_api/WebSocketLib/IConnectionPool.cs
+++ source_api/WebSocketLib/IConnectionPool.cs
@@ -1,13 +1,14 @@
-﻿namespace WebSocketLib
+﻿using System.Collections.Generic;
+using System.Net.WebSockets;
+
+namespace WebSocketLib
 {
     interface IConnectionPool<T>
     {
         bool AddNewConnect();
         bool DeleteConnection();
         bool IsConnect(T id);
-        bool SendBytesTo(T id, byte[] data);
-        bool SendXmlStrTo(T id, string xmlStr);
-        bool SendJsonStrTo(T id, string jsonStr);
+        bool SendTo(T id, object data);
         object Serialization(object o);
         object Deserialization(object o);
     }
