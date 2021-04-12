ba74626bb659e7a250e7aeda1e6a27a2b921c7e3
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2021-04-12 20:00:01 +0700

    lehuynhgiang add code

--- source_clientside/src/assets/js/websocket/WSMediator.js
+++ source_clientside/src/assets/js/websocket/WSMediator.js
@@ -1,11 +1,49 @@
 /**
- * @file websocketHandler.js
+ * @file WSMediator.js
  * @author Giang Le Huynh
  * @since 0.0.1
  */
-const URL_TO_SERVER = "ws://127.0.0.1";
+
 /**
- * 
+ * Define module token
+ */
+const TOKEN_DEF={
+    DEFAULT:0,
+    MODULE1:1,
+    MODULE2:2,
+    MODULE3:3,
+}
+
+/**
+ * Define ws url
+ */
+const WS_URL = "ws://127.0.0.1";
+
+/**
+ * Define mediator class
+ */
+class WSMediator{
+    //#region Fields
+    Clients=[]
+    //#endregion Fields
+
+    //#region Methods
+    register(callbackFunc, token=TOKEN_DEF.DEFAULT){
+        const obj={key:token, callbackFunc:callbackFunc};
+        this.Clients.push(obj);
+    }
+
+    notify(token=TOKEN_DEF.DEFAULT, jsonData=[]){
+        const listNotifiedClients = this.Clients[token];
+        for(obj in listNotifiedClients){
+            obj.callbackFunc(jsonData);
+        }
+    }
+    //#endregion Methods
+}
+
+/**
+ * Define event handler for web socket
  */
 class WebSocketHandler {
     //#region Fields
