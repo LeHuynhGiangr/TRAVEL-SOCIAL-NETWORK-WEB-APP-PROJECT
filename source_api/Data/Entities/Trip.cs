fe806c57d72b4f32add7f79da28860431cd82169
Ngô Đặng Đồng Khởi <ngodangdongkhoi@gmail.com>
2021-05-02 17:14:24 +0700

    ngodangdongkhoi fix datatype of Datetime Trip

--- source_api/Data/Entities/Trip.cs
+++ source_api/Data/Entities/Trip.cs
@@ -20,8 +20,8 @@ namespace Data.Entities
         public string Service { get; set; }
         public string Policy { get; set; }
         public string InfoContact { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateModified { get; set; }
         public Guid UserId { get; set; }
